83c2e4615991b57a9a97dc9e111be47ba4fa96f3
megafatayat <P041121007@bussan.co.id>
2021-11-24 09:31:08 +0700

    [Feature] Able to read data mahasiswa from database

---
+++ application/views/mahasiswa/index.php
@@ -0,0 +1,20 @@
+<div class="container">
+
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <a href="" class="btn btn-primary">Tambah Data Mahasiswa</a>
+        </div>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <h3>Daftar Mahasiswa</h3>
+            <ul class="list-group">
+                <?php foreach ($mahasiswa as $mhs) : ?>
+                    <li class="list-group-item"><?= $mhs['nama']; ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
+
+</div>
